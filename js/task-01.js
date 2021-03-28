275d9cd82f4a48d557f1b27c28de136faa29c84e
Alisa Morenko <Alisa.Morenko@gmail.com>
2021-03-28 22:51:25 +0300

    добавила вариант 2 задачи с map

--- js/task-01.js
+++ js/task-01.js
@@ -4,4 +4,5 @@ console.log(`В списке ${categoriesEl.length} категории.`);
 const categoriesNameEl = document.querySelectorAll('h2');
 for (let i = 0; i < categoriesNameEl.length; i += 1) {
     console.log(`Категория: ${categoriesNameEl[i].textContent}`);
+    
 }
--- js/task-02.js
+++ js/task-02.js
@@ -8,13 +8,21 @@ const ingredients = [
 ];
 
 const liContainerEl = document.querySelector('#ingredients');
-console.log(liContainerEl);
 
-const elements = [];
-for (let i = 0; i < ingredients.length; i += 1) {
-    const liEl = document.createElement('li');
-    liEl.textContent = ingredients[i];
-    elements.push(liEl);
-}
+// const elements = [];
+// for (let i = 0; i < ingredients.length; i += 1) {
+//     const liEl = document.createElement('li');
+//     liEl.textContent = ingredients[i];
+//     elements.push(liEl);
+// }
+// console.log(elements);
+// liContainerEl.append(...elements);
+
+const elements = ingredients.map(ingredient => {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  return liEl
+});
+
 console.log(elements);
 liContainerEl.append(...elements);
