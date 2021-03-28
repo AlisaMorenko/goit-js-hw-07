14336143b68e75199a1cfe8312e637a7fa994edd
Alisa Morenko <Alisa.Morenko@gmail.com>
2021-03-28 15:20:31 +0300

    вторая задача

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const liContainerEl = document.querySelector('#ingredients');
+console.log(liContainerEl);
+
+const elements = [];
+for (let i = 0; i < ingredients.length; i += 1) {
+    const liEl = document.createElement('li');
+    liEl.textContent = ingredients[i];
+    elements.push(liEl);
+}
+console.log(elements);
+liContainerEl.append(...elements);
