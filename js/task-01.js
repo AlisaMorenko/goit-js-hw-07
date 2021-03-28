a6f02994375b57f1064d9c36852841163b401583
Alisa Morenko <Alisa.Morenko@gmail.com>
2021-03-28 13:21:18 +0300

    частично первая решена

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,7 @@
-const add = []
+const categoriesEl = document.querySelectorAll('.item');
+console.log(`В списке ${categoriesEl.length} категории.`);
+
+const categoriesNameEl = document.querySelectorAll('h2');
+for (let i = 0; i < categoriesNameEl.length; i += 1) {
+    console.log(`Категория: ${categoriesNameEl[i].textContent}`);
+}
