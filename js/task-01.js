cbd36693b17cab9c1f91d5af5bcbd58c327350e9
Alisa Morenko <Alisa.Morenko@gmail.com>
2021-04-02 21:07:50 +0300

    доделала 1 задачу

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,11 @@
 const categoriesEl = document.querySelectorAll('.item');
 console.log(`В списке ${categoriesEl.length} категории.`);
 
-const categoriesNameEl = document.querySelectorAll('h2');
-for (let i = 0; i < categoriesNameEl.length; i += 1) {
+
+for (let i = 0; i < categoriesEl.length; i += 1) {
+    const categoriesNameEl = document.querySelectorAll('h2');
     console.log(`Категория: ${categoriesNameEl[i].textContent}`);
-    
-}
+
+    const categoriesElements = categoriesEl[i].querySelector('ul').children.length;
+    console.log(`Количество элементов: ${categoriesElements}`);
+};
