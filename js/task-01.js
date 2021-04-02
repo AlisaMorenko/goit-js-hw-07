cf6f3abfcef6c318530c51aff654eaacb5af196e
Alisa Morenko <Alisa.Morenko@gmail.com>
2021-04-02 23:12:41 +0300

    задача 5

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,11 @@
 const categoriesEl = document.querySelectorAll('.item');
 console.log(`В списке ${categoriesEl.length} категории.`);
 
-
 for (let i = 0; i < categoriesEl.length; i += 1) {
-    const categoriesNameEl = document.querySelectorAll('h2');
-    console.log(`Категория: ${categoriesNameEl[i].textContent}`);
+  const categoriesNameEl = document.querySelectorAll('h2');
+  console.log(`Категория: ${categoriesNameEl[i].textContent}`);
 
-    const categoriesElements = categoriesEl[i].querySelector('ul').children.length;
-    console.log(`Количество элементов: ${categoriesElements}`);
-};
+  const categoriesElements = categoriesEl[i].querySelector('ul').children
+    .length;
+  console.log(`Количество элементов: ${categoriesElements}`);
+}
---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const refs = {
+  input: document.querySelector('#name-input'),
+  output: document.querySelector('#name-output'),
+};
+
+refs.input.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+  if (refs.input.value.length > 0) {
+    refs.output.textContent = refs.input.value;
+  } else {
+    refs.output.textContent = 'незнакомец';
+  }
+}
